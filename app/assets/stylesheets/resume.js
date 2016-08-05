e164be952e1c796cb16e274d6cc64b0635b63e41
Austin Reichardt <areich6@austins-mbp.attlocal.net>
2016-08-05 17:18:25 -0500

    initial commit

---
+++ app/assets/stylesheets/resume.js
@@ -0,0 +1,25 @@
+Quicka than a cruise missile/
+Say the Pentagon's reports/
+Shine like a diamond while I run da city/
+Can't see da haters cuz they dull like quartz/ 
+I win Gold everytime, ain't nobody catchin' up/
+Call me Applicant One, cuz everyone else a runnerup/
+I be siftin' through da code like Wozniak
+If Office Luv have a place for me (uh)/ 
+Don't wait, emails me back/
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
